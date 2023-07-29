681e5f662210c3a00bdb382950b3475d7ebd5f93
Razi Mutussin <razi@lizard-apps.com>
2023-07-29 11:24:05 +0800

    Complete day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("index.txt").unwrap();
+    let elfs: Vec<&str> = file.split("\n\n").collect();
+
+    let mut elfs_cals: Vec<u32> = elfs
+        .iter()
+        .map(|elf| {
+            let calories: Vec<u32> = elf.split("\n").map(|a| a.parse().unwrap_or(0)).collect();
+            calories.iter().sum()
+        })
+        .collect();
+
+    elfs_cals.sort_by(|a, b| b.cmp(a));
+    let top_three_total: u32 = elfs_cals[0..3].iter().sum();
+    println!("{}", top_three_total);
 }
